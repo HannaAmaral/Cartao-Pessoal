c83ff34b9a83afb3cfdbb985f22a67fe211e4f0b
Hanna Amaral <hannaap0001@gmail.com>
2026-08-14 21:07:54 -0300

    error

--- sw.js
+++ sw.js
@@ -1,27 +1,51 @@
-const cacheName = 'ola-pwa';
+const cacheName = 'ola-pwa-v1';
 
-var filesToCache = [
+const filesToCache = [
     './',
-    './manifest.webmanifest',
     './index.html',
+    './manifest.webmanifest',
+    './css/style.css',
     './js/main.js'
-]
+];
 
-self.addEventListener('install',event => {
+// Instala o Service Worker e cria o cache
+self.addEventListener('install', event => {
 
-    event.waitUntill(
+    event.waitUntil(
         caches.open(cacheName)
-        .then(cache=> 
-            cache.addAll(filesToCache) )
-        );
+            .then(cache => {
+                return cache.addAll(filesToCache);
+            })
+    );
+
+    self.skipWaiting();
 });
 
-self.addEventListener( 'fetch',event => {
-    event.respondWih(
-        caches.match(event.request)
-            .then(Response=> {
-                 return Response || fetch(event.request);
-            }
-        )
+// Assume o controle das páginas abertas
+self.addEventListener('activate', event => {
+
+    event.waitUntil(
+        self.clients.claim()
     );
+
+});
+
+// Intercepta as requisições
+self.addEventListener('fetch', event => {
+
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+
+                // Encontrou no cache
+                if (response) {
+                    return response;
+                }
+
+                // Não encontrou → tenta a internet
+                return fetch(event.request);
+
+            })
+    );
+
 });
